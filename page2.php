552f1f5925e44e360bd3826ca589c86190b725b6
Daniel Nguyen <dnguyen85@gmail.com>
2013-02-15 13:32:15 -0800

    hurry up and learn it already

---
+++ page2.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Sciton Marketing Materials</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+    <script type="text/javascript">var switchTo5x=true;</script>
+    <script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
+    <script type="text/javascript" src="http://s.sharethis.com/loader.js"></script>
+  </head>
+  <div class="container">
+  	<div class="row">
+	    <div class="header span12">
+	      <ul class="nav nav-pills pull-right">
+	        <li><a href="#">Sciton Corporate Site</a></li>
+	        <li><a href="#">Contact Us</a></li>
+	      </ul>
+	      <h3 class="muted"><img src="assets/img/sciton-logo1.png" width="190">&nbsp;&nbsp; Marketing Support</h3>
+	      <hr>
+	    </div>
+	  </div>
+  <div class="container" id="content">
+    <div class="row"> 
+      <?php include("sidebar.php"); ?>
+        
+      <div class="span8">
+
+
+          <!-- Thumbnails
+          ================================================== -->
+          <section id="thumbnails">
+            <div class="page-header">
+              <h3>Resurfacing Perfected Marketing Suite<br /><small>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.</small></h>
+            </div>
+            
+     <div class="row-fluid">
+      <ul class="thumbnails">
+        <li class="thumbnail">
+          <a class="" href="#" target="_blank">
+            <img src="assets/img/thumbnail150.jpg" alt="#">
+          </a>
+          <div class="caption">
+          	<a href="#" class="btn btn-primary">PDF</a>
+          	<a href="#" class="btn">JPEG</a>
+          </div>
+        </li>
+        <li class="thumbnail">
+          <a class="" href="#" target="_blank">
+            <img src="assets/img/thumbnail150.jpg" alt="#">
+          </a>
+          <div class="caption">
+          	<a href="#" class="btn btn-primary">PDF</a>
+          	<a href="#" class="btn">JPEG</a>
+          </div>
+        </li>
+        <li class="thumbnail">
+          <a class="" href="#" target="_blank">
+            <img src="assets/img/thumbnail150.jpg" alt="#">
+          </a>
+          <div class="caption">
+          	<a href="#" class="btn btn-primary">PDF</a>
+          	<a href="#" class="btn">JPEG</a>
+          </div>
+        </li>
+        <li class="thumbnail">
+          <a class="" href="#" target="_blank">
+            <img src="assets/img/thumbnail150.jpg" alt="#">
+          </a>
+          <div class="caption">
+          	<a href="#" class="btn btn-primary">PDF</a>
+          	<a href="#" class="btn">JPEG</a>
+          </div>
+        </li>
+      </ul>
+     </div>
+     
+     <div class="row-fluid">
+      <ul class="thumbnails">
+        <li class="thumbnail">
+          <a class="" href="#" target="_blank">
+            <img src="assets/img/thumbnail150.jpg" alt="#">
+          </a>
+          <div class="caption">
+          	<a href="#" class="btn btn-primary">PDF</a>
+          	<a href="#" class="btn">JPEG</a>
+          </div>
+        </li>
+        <li class="thumbnail">
+          <a class="" href="#" target="_blank">
+            <img src="assets/img/thumbnail150.jpg" alt="#">
+          </a>
+          <div class="caption">
+          	<a href="#" class="btn btn-primary">PDF</a>
+          	<a href="#" class="btn">JPEG</a>
+          </div>
+        </li>
+        <li class="thumbnail">
+          <a class="" href="#" target="_blank">
+            <img src="assets/img/thumbnail150.jpg" alt="#">
+          </a>
+          <div class="caption">
+          	<a href="#" class="btn btn-primary">PDF</a>
+          	<a href="#" class="btn">JPEG</a>
+          </div>
+        </li>
+        <li class="thumbnail">
+          <a class="" href="#" target="_blank">
+            <img src="assets/img/thumbnail150.jpg" alt="#">
+          </a>
+          <div class="caption">
+          	<a href="#" class="btn btn-primary">PDF</a>
+          	<a href="#" class="btn">JPEG</a>
+          </div>
+        </li>
+      </ul>
+     </div>
+     
+     <div class="row-fluid">
+      <ul class="thumbnails">
+        <li class="thumbnail">
+          <a class="" href="#" target="_blank">
+            <img src="assets/img/thumbnail150.jpg" alt="#">
+          </a>
+          <div class="caption">
+          	<a href="#" class="btn btn-primary">PDF</a>
+          	<a href="#" class="btn">JPEG</a>
+          </div>
+        </li>
+        <li class="thumbnail">
+          <a class="" href="#" target="_blank">
+            <img src="assets/img/thumbnail150.jpg" alt="#">
+          </a>
+          <div class="caption">
+          	<a href="#" class="btn btn-primary">PDF</a>
+          	<a href="#" class="btn">JPEG</a>
+          </div>
+        </li>
+        <li class="thumbnail">
+          <a class="" href="#" target="_blank">
+            <img src="assets/img/thumbnail150.jpg" alt="#">
+          </a>
+          <div class="caption">
+          	<a href="#" class="btn btn-primary">PDF</a>
+          	<a href="#" class="btn">JPEG</a>
+          </div>
+        </li>
+        <li class="thumbnail">
+          <a class="" href="#" target="_blank">
+            <img src="assets/img/thumbnail150.jpg" alt="#">
+          </a>
+          <div class="caption">
+          	<a href="#" class="btn btn-primary">PDF</a>
+          	<a href="#" class="btn">JPEG</a>
+          </div>
+        </li>
+      </ul>
+     </div>
+     
+     <div class="row-fluid">
+      <ul class="thumbnails">
+        <li class="thumbnail">
+          <a class="" href="#" target="_blank">
+            <img src="assets/img/thumbnail150.jpg" alt="#">
+          </a>
+          <div class="caption">
+          	<a href="#" class="btn btn-primary">PDF</a>
+          	<a href="#" class="btn">JPEG</a>
+          </div>
+        </li>
+        <li class="thumbnail">
+          <a class="" href="#" target="_blank">
+            <img src="assets/img/thumbnail150.jpg" alt="#">
+          </a>
+          <div class="caption">
+          	<a href="#" class="btn btn-primary">PDF</a>
+          	<a href="#" class="btn">JPEG</a>
+          </div>
+        </li>
+        <li class="thumbnail">
+          <a class="" href="#" target="_blank">
+            <img src="assets/img/thumbnail150.jpg" alt="#">
+          </a>
+          <div class="caption">
+          	<a href="#" class="btn btn-primary">PDF</a>
+          	<a href="#" class="btn">JPEG</a>
+          </div>
+        </li>
+        <li class="thumbnail">
+          <a class="" href="#" target="_blank">
+            <img src="assets/img/thumbnail150.jpg" alt="#">
+          </a>
+          <div class="caption">
+          	<a href="#" class="btn btn-primary">PDF</a>
+          	<a href="#" class="btn">JPEG</a>
+          </div>
+        </li>
+      </ul>
+     </div>
+     
+      </div>
+    </div>
+    <hr>
+  </div>
+
+
+    <!-- Footer
+    ================================================== -->
+      <div class="footer">
+        <p><a href="#">Home</a> &#124; <a href="#">Contact Us</a> &#124; <a href="#">Visit Corporate Site</a> &#124; <a href="#">Privacy Policy</a> &#124; <a href="#">Copyright Info</a></p>
+      </div>
+
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
+    <script src="assets/js/script.js"></script>
+
+    <!-- Social Sharing ShareThis
+    ================================================== -->
+<script type="text/javascript">stLight.options({publisher: "ur-c51ab84-c48-b456-c3dd-2fbcd040431e", doNotHash: true, doNotCopy: false, hashAddressBar: false});</script>
+<script>
+var options={ "publisher": "ur-c51ab84-c48-b456-c3dd-2fbcd040431e", "position": "right", "ad": { "visible": false, "openDelay": 5, "closeDelay": 0}, "chicklets": { "items": ["facebook", "twitter", "linkedin", "email"]}};
+var st_hover_widget = new sharethis.widgets.hoverbuttons(options);
+</script>
+
+  </body>
+</html>
---
+++ sidebar.php
@@ -0,0 +1,197 @@
+      <div class="span3 bs-docs-sidebar">
+        <ul class="accordion" id="accordiAwesome">
+          <li class="accordion-group">
+          
+          	<div class="accordion-heading">
+          		<div style="padding:10px;">
+          			<a href="resurfacingperfected.php">Resurfacing Perfected</a>
+          			<a class="accordion-toggle" data-parent="#accordiAwesome" data-toggle="collapse" href="#collapseOne">
+          			
+<?php
+if (stripos($_SERVER['REQUEST_URI'], 'resurfacingperfected.php')){
+     echo '<i class="icon-arrow-up">';
+}
+else{
+     echo '<i class="icon-arrow-down">';
+}
+?>          		
+	
+          			</i></a>
+          		</div>
+          	</div>
+
+<?php
+if (stripos($_SERVER['REQUEST_URI'], 'resurfacingperfected.php')){
+     echo '<div class="accordion-body in collapse" id="collapseOne">';
+}
+else{
+     echo '<div class="accordion-body collapse" id="collapseOne">';
+}
+?>
+
+	          	<ul>
+		          <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	            </ul>
+          	</div>
+          </li>
+          
+          <li class="accordion-group">
+          	<div class="accordion-heading">
+          		<div style="padding:10px;">
+          			<a href="#accordiAwesome">Forever Young BBL</a>
+          			<a class="accordion-toggle" data-parent="#accordiAwesome" data-toggle="collapse" href="#collapseTwo"><i class="icon-arrow-down"></i></a>
+          		</div>
+          	</div>
+          	<div class="accordion-body collapse" id="collapseTwo">
+	          	<ul>
+		          <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	            </ul>
+          	</div>
+          </li>
+
+          <li class="accordion-group">
+          	<div class="accordion-heading">
+          		<div style="padding:10px;">
+          			<a href="#accordiAwesome">Body Contouring</a>
+          			<a class="accordion-toggle" data-parent="#accordiAwesome" data-toggle="collapse" href="#collapseThree"><i class="icon-arrow-down"></i></a>
+          		</div>
+          	</div>
+          	<div class="accordion-body collapse" id="collapseThree">
+	          	<ul>
+		          <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	            </ul>
+          	</div>
+          </li>
+          
+          <li class="accordion-group">
+          	<div class="accordion-heading">
+          		<div style="padding:10px;">
+          			<a href="#accordiAwesome">Hair Removal</a>
+          			<a class="accordion-toggle" data-parent="#accordiAwesome" data-toggle="collapse" href="#collapseFour"><i class="icon-arrow-down"></i></a>
+          		</div>
+          	</div>
+          	<div class="accordion-body collapse" id="collapseFour">
+	          	<ul>
+		          <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	            </ul>
+          	</div>
+          </li>
+          
+          <li class="accordion-group">
+          	<div class="accordion-heading">
+          		<div style="padding:10px;">
+          			<a href="#accordiAwesome">Onychomycosis</a>
+          			<a class="accordion-toggle" data-parent="#accordiAwesome" data-toggle="collapse" href="#collapseFive"><i class="icon-arrow-down"></i></a>
+          		</div>
+          	</div>
+          	<div class="accordion-body collapse" id="collapseFive">
+	          	<ul>
+		          <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	            </ul>
+          	</div>
+          </li>          
+
+          <li class="accordion-group">
+          	<div class="accordion-heading">
+          		<div style="padding:10px;">
+          			<a href="#accordiAwesome">Acne</a>
+          			<a class="accordion-toggle" data-parent="#accordiAwesome" data-toggle="collapse" href="#collapseSix"><i class="icon-arrow-down"></i></a>
+          		</div>
+          	</div>
+          	<div class="accordion-body collapse" id="collapseSix">
+	          	<ul>
+		          <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	            </ul>
+          	</div>
+          </li>  
+          
+          <li class="accordion-group">
+          	<div class="accordion-heading">
+          		<div style="padding:10px;">
+          			<a href="#accordiAwesome">Vascular</a>
+          			<a class="accordion-toggle" data-parent="#accordiAwesome" data-toggle="collapse" href="#collapseSeven"><i class="icon-arrow-down"></i></a>
+          		</div>
+          	</div>
+          	<div class="accordion-body collapse" id="collapseSeven">
+	          	<ul>
+		          <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	            </ul>
+          	</div>
+          </li>  
+
+          <li class="accordion-group">
+          	<div class="accordion-heading">
+          		<div style="padding:10px;">
+          			<a href="#accordiAwesome">Sciton</a>
+          			<a class="accordion-toggle" data-parent="#accordiAwesome" data-toggle="collapse" href="#collapseEight"><i class="icon-arrow-down"></i></a>
+          		</div>
+          	</div>
+          	<div class="accordion-body collapse" id="collapseEight">
+	          	<ul>
+		          <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	            </ul>
+          	</div>
+          </li>  
+          
+          <li class="accordion-group">
+          	<div class="accordion-heading">
+          		<div style="padding:10px;">
+          			<a href="#accordiAwesome">Before and After Photos</a>
+          			<a class="accordion-toggle" data-parent="#accordiAwesome" data-toggle="collapse" href="#collapseNine"><i class="icon-arrow-down"></i></a>
+          		</div>
+          	</div>
+          	<div class="accordion-body collapse" id="collapseNine">
+	          	<ul>
+		          <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	              <li><a href="#">- Type</a></li>
+	            </ul>
+          	</div>
+          </li>
+
+          <li class="accordion-group">
+          	<div class="accordion-heading">
+          		<div style="padding:10px;">
+          			Want to order brochures?<br />
+          			(555) 555-5555<br />
+          			<a href="OrderForm.pdf">Order Form (PDF)</a>
+          		</div>
+          	</div>
+          </li>   
+
+        </ul>
+      </div>
